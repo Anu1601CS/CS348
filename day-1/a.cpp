109a7dc8f419f9aefb56c61f6b2769f9af2fd45c
Anu1601CS <anuragvns1111@gmail.com>
2019-01-20 20:00:20 +0530

    new

--- day-1/a.cpp
+++ day-1/a.cpp
@@ -10,103 +10,100 @@ struct source
 
 int main()
 {
-	int num;
-	double tm;
+	int numSource, i, j, a;
+	double stime;
 	cout<<"Enter the number of sources: ";
-	cin>>num;
+	cin>>numSource;
 	cout<<"Enter sending rate and bandwidth of each source: \n";
-	struct source tmp[num + 1];
-	int i = 1;
-	int cnt[num + 1];
-	double sum[num + 1];
+	struct source tmp[numSource + 1];
+	int arr[numSource + 1];
+	double arr2[numSource + 1];
 
-	for (i = 1; i <= num; i++)
-		sum[i] = 0.0;
+	for (i = 1; i <= numSource; i++)
+		arr2[i] = 0.0;
 
-	memset(cnt, 0, sizeof(cnt));
+	memset(arr, 0, sizeof(arr));
 
-	for (i = 1; i <= num; i++)
+	for (i = 1; i <= numSource; i++)
 	{
-		double x1;
-		int y1;
-		cin>>x1>>y1;
+		double a;
+		int b;
+		cin>>a>>b;
 
-		tmp[i].sending_rate = x1;
-		tmp[i].bandwidth = y1;
+		tmp[i].sending_rate = a;
+		tmp[i].bandwidth = b;
 	}
 
 	cout<<"Enter bandwidth of sink: ";
-	int x1;
-	cin>>x1;
-	int sink_wid = x1;
+	cin>>a;
+	int sink_wid = a;
 	cout<<"Enter packet size: ";
 	int pkt;
 	cin>>pkt;
-	cout<<"Enter simulation time:\n";
-	cin>>tm;
+	cout<<"Enter simulation time: ";
+	cin>>stime;
 
 	priority_queue<pdi, vector<pdi>, greater<pdi>> pq;
-	int j = 1;
 	double snk_sp = ((double)pkt) / ((double)sink_wid);
 
-	for (i = 1; i <= num; i++)
+	for (i = 1; i <= numSource; i++)
 	{
-		int k = tm * (double)tmp[i].sending_rate;
+		int k = stime * (double)tmp[i].sending_rate;
 		double sp = ((double)pkt) / ((double)tmp[i].bandwidth);
-		double x2 = 0.0;
+		double c = 0.0;
 		double k1 = 1 / (tmp[i].sending_rate);
-		cnt[i] = k;
+		arr[i] = k;
 
 		for (j = 1; j <= k; j++)
 		{
 			if (j == 1)
 			{
 				pq.push(make_pair(sp + k1, i));
-				x2 = sp + k1;
-				sum[i] += sp;
+				c = sp + k1;
+				arr2[i] += sp;
 			}
 			else
 			{
-				if (x2 < (double)(j)*k1)
-					x2 = (double)(j)*k1;
+				if (c < (double)(j)*k1)
+					c = (double)(j)*k1;
 				else
-					sum[i] += (abs(x2 - (double)(j) * (double)k1));
-				pq.push(make_pair(x2 + sp, i));
-				sum[i] += sp;
-				x2 += sp;
+					arr2[i] += (abs(c - (double)(j) * (double)k1));
+				pq.push(make_pair(c + sp, i));
+				arr2[i] += sp;
+				c += sp;
 			}
 		}
 	}
 
-	double x2 = 0.0;
+	double c = 0.0;
 	j = 1;
 	double tmp_tm = 0.0;
 
 	while (!pq.empty())
 	{
-		pdi p1 = pq.top();
+		pdi p = pq.top();
 
 		if (j == 1)
 		{
-			sum[p1.second] += (snk_sp);
-			x2 = (snk_sp) + p1.first;
+			arr2[p.second] += (snk_sp);
+			c = (snk_sp) + p.first;
 		}
 		else
 		{
-			if (p1.first >= x2)
+			if (p.first >= c)
 			{
-				sum[p1.second] += (snk_sp);
-				x2 = p1.first;
+				arr2[p.second] += (snk_sp);
+				c = p.first;
 			}
 			else
 			{
-				sum[p1.second] += (snk_sp + abs(x2 - p1.first));
+				arr2[p.second] += (snk_sp + abs(c - p.first));
 			}
 
-			x2 += snk_sp;
+			c += snk_sp;
 		}
 
-		tmp_tm = p1.first;
+		tmp_tm = p.first;
 		j++;
 		pq.pop();
 	}
@@ -114,9 +111,9 @@ int main()
 	cout<<"\nThe average delay of the sources:\n";
 	cout<<"\nSource\t:\tAverage Delay\n";
 
-	for (i = 1; i <= num; i++)
+	for (i = 1; i <= numSource; i++)
 	{
-		cout <<i << "\t:\t" << sum[i] / cnt[i] << "\n";
+		cout <<i << "\t:\t" << arr2[i] / arr[i] << "\n";
 	}
 
 	return 0;
